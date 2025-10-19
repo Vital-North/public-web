5e6c0a9d0ddf1d53f41fc2958f86059b0db3333d
Garth Cumming <garth.cumming@tlcsolutions.ca>
2025-10-19 19:21:48 -0400

    Fix paths for custom domain vitalnorth.net

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://vital-north.github.io',
-  base: '/public-web',
+  site: 'https://vitalnorth.net',
+  base: '/',
   output: 'static',
   vite: {
     plugins: [tailwindcss()]
